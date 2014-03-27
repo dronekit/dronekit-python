c4e6a0310cb02097c7b89e83c7516a3ef20c7988
Kevin Hester <kevinh@geeksville.com>
2014-03-27 09:47:38 -1000

    initial guess at a setup.py script

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, Extension
+import numpy as np
+import platform
+
+version = '0.1.0'
+
+ext_modules = []
+
+setup (name = 'droneapi',
+       zip_safe=True,
+       version = version,
+       description = 'Python language bindings for the DroneApi',
+       long_description = '''Python language bindings for the DroneApi (includes the droneapi MAVProxy module)''',
+       url = 'https://github.com/3drobotics/droneapi-python',
+       author = '3D Robotics',
+       install_requires = [ 'pymavlink',
+                            'MAVProxy' ],
+       author_email = 'kevinh@geeksville.com',
+       classifiers=['Development Status :: 4 - Beta',
+                    'Environment :: Console',
+                    'Intended Audience :: Science/Research',
+                    'License :: OSI Approved :: Apache License version 2',
+                    'Operating System :: OS Independent',
+                    'Programming Language :: Python :: 2.7',
+                    'Topic :: Scientific/Engineering'
+                    ],
+       license='apache',
+       packages = ['dapi_module'],
+       ext_modules = ext_modules)
