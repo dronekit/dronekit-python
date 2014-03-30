f5e44e8f731ee3002b041f00987ff0b5d8d21f82
Kevin Hester <kevinh@geeksville.com>
2014-03-29 19:04:45 -1000

    A minimal API example

--- example/small-demo.py
+++ example/small-demo.py
@@ -1,12 +1,33 @@
+#
+# This is a small example of the python drone API
+# Usage:
+# * mavproxy.py
+# * module load api
+# * api start small-demo.py
+#
 from droneapi.lib import VehicleMode
 
+# First get an instance of the API endpoint
 api = local_connect()
+# get our vehicle - when running with mavproxy it only knows about one vehicle (for now)
 v = api.get_vehicles()[0]
+# Print out some interesting stats about the vehicle
 print "Mode: %s" % v.mode
 print "Location: %s" % v.location
 print "Attitude: %s" % v.attitude
 print "GPS: %s" % v.gps_0
+
+# You can read and write parameters
 print "Param: %s" % v.parameters['THR_MAX']
-print "Home WP: %s" % v.commands[0]
+
+# Now download the vehicle waypoints
+cmds = v.commands
+cmds.download()
+cmds.wait_valid()
+print "Home WP: %s" % cmds[0]
+
+# Now change the vehicle into auto mode
 v.mode = VehicleMode("AUTO")
+
+# Always call flush to guarantee that previous writes to the vehicle have taken place
 v.flush()
