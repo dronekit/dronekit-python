ec967dd61c8efa048ee1a40dca1f566559a8bf87
Kevin Hester <kevinh@geeksville.com>
2014-05-25 09:52:21 -0700

    Add test-case/example for sending raw mavlink:
    +msg = v.message_factory.command_long_encode(0, 0,
    +                                  mavutil.mavlink.MAV_CMD_CONDITION_YAW, 0,
    +                                  0, 0, 0, 0, 1, 0, 0)
    +print "Created msg: %s" % msg
    +v.send_mavlink(msg)
    
    When run the following output is expected:
    
    AUTO> api start small_demo.py
    Mode: VehicleMode:AUTO
    Location: Location:lat=37.5210376,lon=-122.3092347,alt=152.720001221,is_relative=True
    Attitude: Attitude:-0.181670248508,-2.71582651138,0.0766400620341
    GPS: GPSInfo:fix=3,num_sat=7
    Param: 75.0
    waiting for download
    AUTO> Requesting 10 waypoints t=Sun May 25 09:51:00 2014 now=Sun May 25 09:51:00 2014
    Home WP: MISSION_ITEM {target_system : 255, target_component : 0, seq : 0, frame : 0, command : 16, current : 0, autocontinue : 1, param1 : 0.0, param2 : 0.0, param3 : 0.0, param4 : 0.0, x : 37.5210456848, y : -122.309158325, z : 151.539993286}
    Current dest: 1
    Created msg: COMMAND_LONG {target_system : 0, target_component : 0, command : 115, confirmation : 0, param1 : 0, param2 : 0, param3 : 0, param4 : 0, param5 : 1, param6 : 0, param7 : 0}
    APIThread-0 exiting...
    Got MAVLink msg: COMMAND_ACK {command : 115, result : 3}
    APM: command received:

--- example/small_demo.py
+++ example/small_demo.py
@@ -6,6 +6,7 @@
 # * api start small-demo.py
 #
 from droneapi.lib import VehicleMode
+from pymavlink import mavutil
 
 # First get an instance of the API endpoint
 api = local_connect()
@@ -26,6 +27,29 @@ cmds.download()
 cmds.wait_valid()
 print "Home WP: %s" % cmds[0]
 print "Current dest: %s" % cmds.next
+
+# Test custom commands
+# Note: For mavlink messages that include a target_system & target_component, those values
+# can just be filled with zero.  The API will take care of using the correct values
+# For instance, from the xml for command_long:
+#                Send a command with up to seven parameters to the MAV
+#
+#                target_system             : System which should execute the command (uint8_t)
+#                target_component          : Component which should execute the command, 0 for all components (uint8_t)
+#                command                   : Command ID, as defined by MAV_CMD enum. (uint16_t)
+#                confirmation              : 0: First transmission of this command. 1-255: Confirmation transmissions (e.g. for kill command) (uint8_t)
+#                param1                    : Parameter 1, as defined by MAV_CMD enum. (float)
+#                param2                    : Parameter 2, as defined by MAV_CMD enum. (float)
+#                param3                    : Parameter 3, as defined by MAV_CMD enum. (float)
+#                param4                    : Parameter 4, as defined by MAV_CMD enum. (float)
+#                param5                    : Parameter 5, as defined by MAV_CMD enum. (float)
+#                param6                    : Parameter 6, as defined by MAV_CMD enum. (float)
+#                param7                    : Parameter 7, as defined by MAV_CMD enum. (float)
+msg = v.message_factory.command_long_encode(0, 0,
+                                  mavutil.mavlink.MAV_CMD_CONDITION_YAW, 0,
+                                  0, 0, 0, 0, 1, 0, 0)
+print "Created msg: %s" % msg
+v.send_mavlink(msg)
 
 # Now change the vehicle into auto mode
 v.mode = VehicleMode("AUTO")
