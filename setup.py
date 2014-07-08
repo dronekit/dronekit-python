c599781fd9896ace7173a367eeb7becb58a33dd2
Kevin Hester <kevinh@geeksville.com>
2014-07-07 20:24:21 -0700

    release 0.1.7: depend on new mavproxy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension
 import numpy as np
 import platform
 
-version = '0.1.6'
+version = '0.1.7'
 
 ext_modules = []
 
@@ -14,7 +14,7 @@ setup (name = 'droneapi',
        url = 'https://github.com/diydrones/droneapi-python',
        author = '3D Robotics',
        install_requires = [ 'pymavlink',
-                            'MAVProxy >= 1.3.1',
+                            'MAVProxy >= 1.3.7',
                             'protobuf >= 2.5.0' ],
        author_email = 'kevinh@geeksville.com',
        classifiers=['Development Status :: 4 - Beta',
