6951a3a205450913e7db2153b702208f12368b87
Matteo Ruina <matteo.ruina@gmail.com>
2015-06-01 10:35:45 +0000

    Fix #137

--- droneapi/lib/__init__.py
+++ droneapi/lib/__init__.py
@@ -173,17 +173,20 @@ class Battery(object):
     """
     Battery info available
 
-    :param battery_voltage: Battery voltage in millivolts
-    :param battery_current: Battery current, in 10 * milliamperes, -1: autopilot does not measure the current
-    :param battery_level: Remaining battery energy, -1: autopilot estimate the remaining battery
+    :param voltage: Battery voltage in millivolts
+    :param current: Battery current, in 10 * milliamperes, -1: autopilot does not measure the current
+    :param level: Remaining battery energy, -1: autopilot estimate the remaining battery
     """
     def __init__(self, voltage, current, level):
-        self.voltage = float(voltage) / 1000.0
-        if current != -1:
-            self.current = float(current) / 100.0
+        self.voltage = voltage / 1000.0
+        if current == -1:
+            self.current = None
         else:
-            self.current = current
-        self.level = level
+            self.current = current / 100.0
+        if level == -1:
+            self.level = None
+        else:
+            self.level = level
 
     def __str__(self):
         return "Battery voltage: {}, current: {}, level: {}".format(self.voltage, self.current, self.level)
