8540d6360ed17efa5d5f0f02a8870cfb9796db36
Matteo Ruina <matteo.ruina@gmail.com>
2015-06-02 07:42:44 +0000

    Better docstring Fix __str__ style

--- droneapi/lib/__init__.py
+++ droneapi/lib/__init__.py
@@ -173,9 +173,9 @@ class Battery(object):
     """
     Battery info available
 
-    :param voltage: Battery voltage in millivolts
-    :param current: Battery current, in 10 * milliamperes, -1: autopilot does not measure the current
-    :param level: Remaining battery energy, -1: autopilot estimate the remaining battery
+    :param voltage: Battery voltage in millivolts.
+    :param current: Battery current, in 10 * milliamperes. ``None`` if the autopilot does not support current measurement.
+    :param level: Remaining battery energy. ``None`` if the autopilot cannot estimate the remaining battery.
     """
     def __init__(self, voltage, current, level):
         self.voltage = voltage / 1000.0
@@ -189,7 +189,7 @@ class Battery(object):
             self.level = level
 
     def __str__(self):
-        return "Battery voltage: {}, current: {}, level: {}".format(self.voltage, self.current, self.level)
+        return "Battery:voltage={},current={},level={}".format(self.voltage, self.current, self.level)
 
 
 class VehicleMode(object):
