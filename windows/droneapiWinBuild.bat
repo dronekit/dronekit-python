4c74450ebd9f13d8133ea6223be7a26ececc9f9d
Stephen Dade <stephen_dade@hotmail.com>
2015-06-13 17:27:04 +1000

    Created windows installer. Use batch file to run it

---
+++ windows/droneapiWinBuild.bat
@@ -0,0 +1,16 @@
+rem build the standalone Dronekit.exe for Windows.
+rem This assumes Python is installed in C:\Python27
+SETLOCAL enableextensions
+
+rem get the version
+for /f "tokens=*" %%a in (
+ 'python returnVersion.py'
+ ) do (
+ set VERSION=%%a
+ )
+
+
+rem -----Build the Installer-----
+"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" /dMyAppVersion=%VERSION% -compile droneapi_installer.iss
+
+pause
