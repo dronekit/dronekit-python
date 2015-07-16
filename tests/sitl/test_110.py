de4b39b58ff7c40d3fde853719bcd4f3b9d61ba9
Tim Ryan <tim@3drobotics.com>
2015-07-16 15:18:59 -0700

    Ensures observers can't be added twice. Fixes #110

--- tests/sitl/test_110.py
+++ tests/sitl/test_110.py
@@ -3,6 +3,7 @@ from pymavlink import mavutil
 import time
 import sys
 import os
+from testlib import assert_equals
 
 def test_110(local_connect):
     api = local_connect()
@@ -15,35 +16,41 @@ def test_110(local_connect):
     time.sleep(3)
 
     # Define example callback for mode
-    allow_callback = True
-    bad_call = [False]
     def armed_callback(attribute):
-        if not allow_callback:
-            bad_call[0] = True
+        armed_callback.called += 1
+    armed_callback.called = 0
 
-    # Only one of the same observer fn should be added.
+    # When the same (event, callback) pair is passed to add_attribute_observer,
+    # only one instance of the observer callback should be added.
     v.add_attribute_observer('armed', armed_callback)
     v.add_attribute_observer('armed', armed_callback)
     v.add_attribute_observer('armed', armed_callback)
     v.add_attribute_observer('armed', armed_callback)
-    allow_callback = True
+    v.add_attribute_observer('armed', armed_callback)
 
     # Disarm and see update.
     v.armed = False
     v.flush()
-
+    # Wait for ACK.
     time.sleep(3)
 
-    # Rmove (all) observers.
+    # Ensure the callback was called.
+    assert armed_callback.called > 0, "Callback should have been called."
+
+    # Rmove all observers. The first call should remove all listeners
+    # we've added; the second call should be ignored and not throw.
+    # NOTE: We test if armed_callback were treating adding each additional callback
+    # and remove_attribute_observer were removing them one at a time; in this
+    # case, there would be three callbacks still attached.
     v.remove_attribute_observer('armed', armed_callback)
     v.remove_attribute_observer('armed', armed_callback)
-    allow_callback = False
+    callcount = armed_callback.called
 
     # Re-arm and see update.
     v.armed = True
     v.flush()
-
+    # Wait for ack
     time.sleep(3)
 
-    # Make sure no bad calls happened.
-    assert not bad_call[0], "Callback should not have been called once removed."
+    # Ensure the callback was called zero times.
+    assert_equals(armed_callback.called, callcount, "Callback should not have been called once removed.")
