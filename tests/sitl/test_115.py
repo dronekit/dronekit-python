dd8e0d8157cdc87e90fa4cbbc72b9e953c0609eb
Tim Ryan <tim@3drobotics.com>
2015-07-27 13:44:52 -0700

    Updates test_115 to use unset_mavlink_callback and stricter checks

--- tests/sitl/test_115.py
+++ tests/sitl/test_115.py
@@ -3,35 +3,43 @@ from pymavlink import mavutil
 import time
 import sys
 import os
+from testlib import assert_equals
 
 def test_115(local_connect):
     api = local_connect()
     v = api.get_vehicles()[0]
 
     # Dummy callback
-    def noop(*args):
-        pass
+    def mavlink_callback(*args):
+        mavlink_callback.count += 1
+    mavlink_callback.count = 0
 
     # Set the callback.
-    v.set_mavlink_callback(noop)
-    
+    v.set_mavlink_callback(mavlink_callback)
+
     # Change the vehicle into STABILIZE mode
     v.mode = VehicleMode("STABILIZE")
-
     # NOTE wait crudely for ACK on mode update
     time.sleep(3)
 
-    # Unset the callback.
-    v.set_mavlink_callback(None)
+    # Expect the callback to have been called
+    assert mavlink_callback.count > 0
 
-    # Disarm should not throw errors.
+    # Unset the callback.
+    v.unset_mavlink_callback()
+    savecount = mavlink_callback.count
+
+    # Disarm. A callback of None should not throw errors
     v.armed = False
     v.flush()
-
+    # NOTE wait crudely for ACK on mode update
     time.sleep(3)
+
+    # Expect the callback to have been called
+    assert_equals(savecount, mavlink_callback.count)
 
     # Re-arm should not throw errors.
     v.armed = True
     v.flush()
-
+    # NOTE wait crudely for ACK on mode update
     time.sleep(3)
