121f33d8a54878da8cb049ed9a504ee39eeab2c1
Tim Ryan <tim@3drobotics.com>
2015-07-27 11:56:01 -0700

    Comments out print lines from test_goto (still useful)

--- tests/sitl/test_goto.py
+++ tests/sitl/test_goto.py
@@ -21,23 +21,23 @@ def test_goto(local_connect):
         Arms vehicle and fly to aTargetAltitude.
         """
 
-        print "Basic pre-arm checks"
+        # print "Basic pre-arm checks"
         # Don't let the user try to fly autopilot is booting
         if vehicle.mode.name == "INITIALISING":
-            print "Waiting for vehicle to initialise"
+            # print "Waiting for vehicle to initialise"
             time.sleep(1)
         while vehicle.gps_0.fix_type < 2:
-            print "Waiting for GPS...:", vehicle.gps_0.fix_type
+            # print "Waiting for GPS...:", vehicle.gps_0.fix_type
             time.sleep(1)
             
-        print "Arming motors"
+        # print "Arming motors"
         # Copter should arm in GUIDED mode
         vehicle.mode    = VehicleMode("GUIDED")
         vehicle.flush()
 
         i = 60
         while not api.exit and vehicle.mode.name != 'GUIDED' and i > 0:
-            print " Waiting for guided %s seconds..." % (i,)
+            # print " Waiting for guided %s seconds..." % (i,)
             time.sleep(1)
             i = i - 1
 
@@ -46,7 +46,7 @@ def test_goto(local_connect):
 
         i = 60
         while not api.exit and not vehicle.armed and vehicle.mode.name == 'GUIDED' and i > 0:
-            print " Waiting for arming %s seconds..." % (i,)
+            # print " Waiting for arming %s seconds..." % (i,)
             time.sleep(1)
             i = i - 1
 
@@ -54,7 +54,7 @@ def test_goto(local_connect):
         assert vehicle.armed
         assert_equals(vehicle.mode.name, 'GUIDED')
 
-        print "Taking off!"
+        # print "Taking off!"
         vehicle.commands.takeoff(aTargetAltitude) # Take off to target altitude
         vehicle.flush()
 
@@ -62,18 +62,18 @@ def test_goto(local_connect):
         # processing the goto (otherwise the command after
         # Vehicle.commands.takeoff will execute immediately).
         while not api.exit:
-            print " Altitude: ", vehicle.location.alt
+            # print " Altitude: ", vehicle.location.alt
             # Test for altitude just below target, in case of undershoot.
             if vehicle.location.alt >= aTargetAltitude * 0.95: 
-                print "Reached target altitude"
-                break;
+                # print "Reached target altitude"
+                break
 
             assert_equals(vehicle.mode.name, 'GUIDED')
             time.sleep(1)
 
     arm_and_takeoff(10)
 
-    print "Going to first point..."
+    # print "Going to first point..."
     point1 = Location(-35.361354, 149.165218, 20, is_relative=True)
     vehicle.commands.goto(point1)
     vehicle.flush()
@@ -81,7 +81,7 @@ def test_goto(local_connect):
     # sleep so we can see the change in map
     time.sleep(3)
 
-    print "Going to second point..."
+    # print "Going to second point..."
     point2 = Location(-35.363244, 149.168801, 20, is_relative=True)
     vehicle.commands.goto(point2)
     vehicle.flush()
@@ -89,6 +89,6 @@ def test_goto(local_connect):
     # sleep so we can see the change in map
     time.sleep(3)
 
-    print "Returning to Launch"
+    # print "Returning to Launch"
     vehicle.mode    = VehicleMode("RTL")
     vehicle.flush()
