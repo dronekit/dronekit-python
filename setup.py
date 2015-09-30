09db97ce77d784330c6dc1789b639364b5329c9b
Tim Ryan <tim@3drobotics.com>
2015-09-30 14:34:42 -0700

    Assign a dedicated, public version.

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup, Extension
 import platform
 
-version = '1.5.0'
+version = '2.0.0b2'
 
-ext_modules = []
-
-setup (name = 'droneapi',
+setup (name = 'dronekit',
        zip_safe=True,
        version = version,
        description = 'Python language bindings for the DroneApi',
-       long_description = '''Python language bindings for the DroneApi''',
-       url = 'https://github.com/diydrones/droneapi-python',
+       long_description = 'Python language bindings for the DroneApi',
+       url = 'https://github.com/dronekit/dronekit-python',
        author = '3D Robotics',
        install_requires = [ 'pymavlink >= 1.1.50',
                             'protobuf >= 2.5.0',
@@ -25,6 +23,5 @@ setup (name = 'droneapi',
                     'Topic :: Scientific/Engineering'
                     ],
        license='apache',
-       packages = ['droneapi', 'droneapi.module', 'droneapi.lib' ],
-       # doesn't work: package_data={'droneapi': ['examples/*']},
-       ext_modules = ext_modules)
+       packages = ['dronekit', 'dronekit.module', 'dronekit.lib' ],
+       ext_modules = [])
