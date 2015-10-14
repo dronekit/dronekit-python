1fb01fbf28fda8ccbfa76f3ff76c3f711f207ade
Tim Ryan <tim@3drobotics.com>
2015-10-14 08:23:39 -0400

    Removes protobuf dependency. Closes #339

--- setup.py
+++ setup.py
@@ -3,25 +3,29 @@ import platform
 
 version = '2.0.0b3'
 
-setup (name = 'dronekit',
-       zip_safe=True,
-       version = version,
-       description = 'Python language bindings for the DroneApi',
-       long_description = 'Python language bindings for the DroneApi',
-       url = 'https://github.com/dronekit/dronekit-python',
-       author = '3D Robotics',
-       install_requires = ['pymavlink >= 1.1.62',
-                            'protobuf >= 2.5.0',
-                            'requests == 2.5.1' ],
-       author_email = 'tim@3drobotics.com, kevinh@geeksville.com',
-       classifiers=['Development Status :: 4 - Beta',
-                    'Environment :: Console',
-                    'Intended Audience :: Science/Research',
-                    'License :: OSI Approved :: Apache Software License',
-                    'Operating System :: OS Independent',
-                    'Programming Language :: Python :: 2.7',
-                    'Topic :: Scientific/Engineering'
-                    ],
-       license='apache',
-       packages = ['dronekit', 'dronekit.module', 'dronekit.lib' ],
-       ext_modules = [])
+setup(name='dronekit',
+      zip_safe=True,
+      version=version,
+      description='Python language bindings for the DroneApi',
+      long_description='Python language bindings for the DroneApi',
+      url='https://github.com/dronekit/dronekit-python',
+      author='3D Robotics',
+      install_requires=[
+          'pymavlink >= 1.1.62',
+          'requests == 2.5.1',
+      ],
+      author_email='tim@3drobotics.com, kevinh@geeksville.com',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Scientific/Engineering',
+      ],
+      license='apache',
+      packages=[
+          'dronekit', 'dronekit.module', 'dronekit.lib'
+      ],
+      ext_modules=[])
--- windows/droneapiWinBuild.bat
+++ windows/droneapiWinBuild.bat
@@ -9,29 +9,5 @@ for /f "tokens=*" %%a in (
  set VERSION=%%a
  )
 
-rem -----Get protobuf and unzip-----
-C:\python27\scripts\pip.exe install --download .\ protobuf
-rem get the protobuf fullname
-for /f "tokens=*" %%a in (
- 'dir /B protobuf-*'
- ) do (
- set PROTOBUFFULL=%%a
- )
-
-rem Use 7zip to unzip files. Works for both 32 and 64 bit versions
-"C:\Program Files\7-zip\7z.exe" x -y %PROTOBUFFULL%
-"C:\Program Files\7-zip\7z.exe" x -y .\dist\protobuf*
-"C:\Program Files (x86)\7-zip\7z.exe" x -y %PROTOBUFFULL%
-"C:\Program Files (x86)\7-zip\7z.exe" x -y .\dist\protobuf*
-
-rem get the protobuf fullname
-for /f "tokens=*" %%a in (
- 'dir /A:D /B protobuf-*'
- ) do (
- set PROTOBUFDIR=%%a
- )
-xcopy /E /Y %PROTOBUFDIR%\google .\google\
-
-
 rem -----Build the Installer-----
 "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" /dMyAppVersion=%VERSION% dronekit_installer.iss
