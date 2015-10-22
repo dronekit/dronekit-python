41b39891a91e81d05b7425d68ba6703500411889
Tim Ryan <tim@3drobotics.com>
2015-10-22 16:30:08 -0700

    Fixes VehicleMode test.

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -1,18 +1,16 @@
 import mock
 from mock import MagicMock
-from nose.tools import eq_, assert_not_equal
-import droneapi
-from droneapi.module.api import APIModule
-from droneapi.lib import VehicleMode
-from nose.tools import assert_equals
+import dronekit
+from dronekit.lib import VehicleMode
+from dronekit import FakeAPI
+from nose.tools import assert_equals, assert_not_equals
 
-def test_get_vehicles():
-    api = APIModule(MagicMock())
-    res = api.get_connection()
-    eq_(len(res.get_vehicles()), 1)
+def test_mode():
+    api = FakeAPI(MagicMock())
+    assert_equals(len(api.get_vehicles()), 1)
 
 def test_vehicle_mode_eq():
-    eq_(VehicleMode('GUIDED'), VehicleMode('GUIDED'))
+    assert_equals(VehicleMode('GUIDED'), VehicleMode('GUIDED'))
 
 def test_vehicle_mode_neq():
-    assert_not_equal(VehicleMode('AUTO'), VehicleMode('GUIDED'))
+    assert_not_equals(VehicleMode('AUTO'), VehicleMode('GUIDED'))
