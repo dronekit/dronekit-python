ed56dda2e9214c48fbcda795b4c2b68bd73cc28e
Hamish Willee <hamishwillee@gmail.com>
2015-11-09 17:12:54 +1100

    Addition file changes (this should have been part of last commit)

--- dronekit/lib/__init__.py
+++ dronekit/lib/__init__.py
@@ -1052,10 +1052,29 @@ class Vehicle(HasObservers):
 
     @property
     def system_status(self):
+        """
+        System status flag according to the MAVLink 
+        `MAV_STATE <http://mavlink.org/messages/common#MAV_STATE_UNINIT>`_ enum.
+        
+        States include:
+        
+        * ``MAV_STATE_UNINIT`` (0): Uninitialized system, state is unknown.
+        * ``MAV_STATE_BOOT`` (1): System is booting up.
+        * ``MAV_STATE_CALIBRATING`` (2): System is calibrating and not flight-ready.
+        * ``MAV_STATE_STANDBY`` (3): System is grounded and on standby. It can be launched any time.
+        * ``MAV_STATE_ACTIVE`` (4): System is active and might be already airborne. Motors are engaged.
+        * ``MAV_STATE_CRITICAL`` (5): System is in a non-normal flight mode. It can however still navigate.
+        * ``MAV_STATE_EMERGENCY`` (6): System is in a non-normal flight mode. It lost control over parts 
+          or over the whole airframe. It is in mayday and going down.
+        * ``MAV_STATE_POWEROFF`` (7): System just initialized its power-down sequence, will shut down now.
+        """
         return self._system_status
 
     @property
     def heading(self):
+        """
+        Current heading in degrees (0..360, where North = 0).
+        """
         return self._heading
 
     @property
