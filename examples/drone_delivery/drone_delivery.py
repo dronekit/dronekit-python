2ef81d4dba608e1980b0002e14b5b9f6d36286cc
Tim Ryan <tim@3drobotics.com>
2015-11-10 16:16:18 -0800

    Improves drone delivery example, also cleanup.

--- examples/drone_delivery/drone_delivery.py
+++ examples/drone_delivery/drone_delivery.py
@@ -40,52 +40,14 @@ cherrypy_conf = {
 }
 
 
-def arm_and_takeoff(aTargetAltitude):
-    """
-    Arms vehicle and fly to aTargetAltitude.
-    """
-
-    print "Basic pre-arm checks"
-    # Don't let the user try to fly autopilot is booting
-    if vehicle.mode.name == "INITIALISING":
-        print "Waiting for vehicle to initialise"
-        time.sleep(1)
-    while vehicle.gps_0.fix_type < 2:
-        print "Waiting for GPS...:", vehicle.gps_0.fix_type
-        time.sleep(1)
-
-    print "Arming motors"
-    # Copter should arm in GUIDED mode
-    vehicle.mode    = VehicleMode("GUIDED")
-    vehicle.armed   = True
-    vehicle.flush()
-
-    while not vehicle.armed:
-        print " Waiting for arming..."
-        time.sleep(1)
-
-    print "Taking off!"
-    vehicle.commands.takeoff(aTargetAltitude) # Take off to target altitude
-    vehicle.flush()
-
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command 
-    #  after Vehicle.commands.takeoff will execute immediately).
-    while True:
-        print " Altitude: ", vehicle.location.alt
-        if vehicle.location.alt>=aTargetAltitude*0.95: #Just below target, in case of undershoot.
-            print "Reached target altitude"
-            break;
-        time.sleep(1)
-
 class Drone(object):
     def __init__(self, home_coords, server_enabled=True):
         self.gps_lock = False
         self.altitude = 30.0
 
         # Connect to the Vehicle
-        print 'Connecting to vehicle on: %s' % args.connect
+        self._log('Connected to vehicle.')
         self.vehicle = vehicle
-        print "connected ..."
         self.commands = self.vehicle.commands
         self.current_coords = []
         self.home_coords = home_coords
@@ -93,19 +55,15 @@ class Drone(object):
         self._log("DroneDelivery Start")
 
         # Register observers
-        self.vehicle.add_attribute_listener('armed', self.armed_callback)
         self.vehicle.add_attribute_listener('location', self.location_callback)
-        self.vehicle.add_attribute_listener('gps_0', self.gps_callback)
-
-        self._log("Waiting for GPS Lock")
 
     def launch(self):
-        while not self.gps_lock:
-            time.sleep(1)
+        self._log("Waiting for ability to arm...")
+        while not self.vehicle.is_armable:
+            time.sleep(.1)
 
         self._log('Running initial boot sequence')
         self.change_mode('GUIDED')
-        time.sleep(5) # EKF warmup
         self.arm()
         self.takeoff()
 
@@ -119,14 +77,12 @@ class Drone(object):
         
     def arm(self, value=True):
         if value:
-            self._log("Arming")
+            self._log('Waiting for arming...')
             self.vehicle.armed = True    
             while not self.vehicle.armed:
-                print " Waiting for arming..."
-                print " Mode: %s" % self.vehicle.mode.name
-                time.sleep(1)              
+                time.sleep(.1)
         else:
-            self._log("Disarming")
+            self._log("Disarming!")
             self.vehicle.armed = False
 
     def _run_server(self):
@@ -143,13 +99,11 @@ class Drone(object):
         cherrypy.engine.start()
 
     def change_mode(self, mode):
-        self._log("Mode: {0}".format(mode))
+        self._log("Changing to mode: {0}".format(mode))
 
         self.vehicle.mode = VehicleMode(mode)
         while self.vehicle.mode.name != mode:
-            print " Waiting for mode change ... (%s)" % self.vehicle.mode.name
-            print "  Mode: {0}".format(mode)
-            self.vehicle.mode = VehicleMode(mode)
+            self._log('  ... polled mode: {0}'.format(mode))
             time.sleep(1)
 
     def goto(self, location, relative=None):
@@ -172,19 +126,6 @@ class Drone(object):
             self.altitude = location.alt
 
         self.current_location = location
-
-    def armed_callback(self, vehicle, name, armed):
-        self._log("DroneDelivery Armed Callback: %s" % armed)
-        self.vehicle.remove_attribute_listener('armed', self.armed_callback)
-
-    def gps_callback(self, vehicle, name, gps):
-        self._log("GPS: {0}".format(gps))
-        if self.vehicle.gps_0.fix_type >= 2:
-            self.gps_lock = True        
-            self.vehicle.remove_attribute_listener('gps_0', self.gps_callback)
-        else:            
-            print "Waiting for GPS...:", vehicle.gps_0.fix_type      
-            self.gps_lock = False
 
     def _log(self, message):
         print "[DEBUG]: {0}".format(message)
