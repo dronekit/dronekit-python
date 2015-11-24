27ddf9003237548f51279f212d0f2cc19e688c15
Tim Ryan <tim@3drobotics.com>
2015-11-24 00:10:15 -0500

    2.0.0

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 
-cd $(dirname $0)
-cd ..
-
-rm -r /tmp/autodocs
-set -e
-python ./setup.py build
-echo Switching to doc tree
-cd docs
-make html
-cp -a _build/html /tmp/autodocs
-cd ..
-git checkout gh-pages
-cp -a /tmp/autodocs/* .
-set +e
-find . | xargs git add
-set -e
-git commit -m "Update docs"
-git push
-git checkout -f master
-
+ssh dronekit.io "./update_python.sh"
