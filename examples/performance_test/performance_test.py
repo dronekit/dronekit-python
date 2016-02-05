1a112bf017d808225d93368d3c6502907b36dff5
Hamish Willee <hamishwillee@gmail.com>
2016-02-05 16:54:46 +1100

    Fix/simplify and document the performance test.

--- examples/performance_test/performance_test.py
+++ examples/performance_test/performance_test.py
@@ -1,124 +1,44 @@
-#
-# This is a small example of the python drone API
-#
-from dronekit import connect, VehicleMode
+"""
+performance_test.py: 
+
+This performance test logs the interval between messages being 
+sent by Dronekit-Python and an acknowledgment being received 
+from the autopilot. It provides a running report of the maximum, 
+minimum, and most recent interval for 30 seconds.
+
+Full documentation is provided at http://python.dronekit.io/examples/performance_test.html
+"""
+from dronekit import connect
 from pymavlink import mavutil
 import time
+import sys
 from datetime import datetime
-import traceback
 
-from matplotlib import pyplot
-from numpy import arange
-import bisect
-
-findings = """
-Reception latency:
-* due to a top level select() on the udp port, the latency for calling process_master seems to be sub 1ms (limit of python timer resolution)
-
-Reception periodicity:
-TBD
-
-Background processing perodicity:
-TBD
-
-Sending perodicity:
-TBD
-
-Computation efficency:
-FIXME
-
-Max closed loop rate:
-* 20ms+/-300us when talking to SITL (every time we recieve a cmd_ack we immediately send a pair of ROI related msgs)
-* The less than 300us variablity makes me think SITL has some 20ms poll rate - need to try with real vehicle
-
-SITL copter load
-Interval (sec) 0.019865
-MaxInterval (sec) 0.021927
-MinInterval (sec) 0.018421
-
-AVR plane load: 20ms+/-7ms
-Interval 0.02061
-MaxInterval 0.025496
-MinInterval 0.011533
-
-PX4 quad load on Edsion: 20ms +60ms -5ms (VERY HIGH VARIABILITY - mostly due to px4 side - see below)
-Interval 0.0281970000001
-MaxInterval 0.0786720000001
-MinInterval 0.0161290000001
-
-PX4 quad load on a pixhawk (a9defa35) talking to my desktop - similar variability as with an Edison:
-Interval 0.01989
-MaxInterval 0.0688479999999
-MinInterval 0.00722900000005
-Interval 0.019929
-MaxInterval 0.0688479999999
-MinInterval 0.00722900000005
-Interval 0.0189700000001
-MaxInterval 0.0688479999999
-MinInterval 0.00722900000005
-
-or here's 20ish of the interval values seen on the px4 (a9defa35) Test
-Interval 0.020012
-Interval 0.0199689999999
-Interval 0.0229640000002
-Interval 0.0171049999999
-Interval 0.0198150000001
-Interval 0.0211049999998
-Interval 0.0199740000003
-Interval 0.0199459999999
-Interval 0.0199590000002
-Interval 0.0200379999997
-Interval 0.0200850000001
-Interval 0.0198839999998
-Interval 0.0200420000001
-Interval 0.0199539999999
-Interval 0.0200760000002
-Interval 0.0199029999999
-Interval 0.0200950000003
-Interval 0.0517199999999
-
-now testing with a plane load with a px4 (a9defa35) at 56kbps - highly variable 25 to 82ms
-Interval 0.0589850000001
-MaxInterval 0.0829760000001
-MinInterval 0.0258819999999
-
-but change to 115kbps and things are much better
-Interval 0.0201160000001
-MaxInterval 0.044656
-MinInterval 0.0150279999998
-
-and changing things to 500kbps everything is just peachie - 18ms
-Interval 0.018119
-MaxInterval 0.02527
-MinInterval 0.015737
-
-Recommendations:
-Run link as fast as you can 1500kbps?
-Turn on hw flow control (and use --rtscts on mavproxy)
-
-mavproxy.py --master=/dev/ttyMFD1,115200 --cmd="api start perf_test.py"
-"""
 
 #Set up option parsing to get connection string
 import argparse  
-parser = argparse.ArgumentParser(description='Example that demonstrates mission import/export from a file. Connects to SITL on local PC by default.')
-parser.add_argument('--connect', default='127.0.0.1:14550',
-                   help="vehicle connection target. Default '127.0.0.1:14550'")
+parser = argparse.ArgumentParser(description='Generates max, min and current interval between message sent and ack recieved. Will start and connect to SITL if no connection string specified.')
+parser.add_argument('--connect', 
+                   help="vehicle connection target string. If not specified, SITL automatically started and used.")
 args = parser.parse_args()
+
+connection_string=args.connect
+
+if not args.connect:
+    print "Starting copter simulator (SITL)"
+    from dronekit_sitl import SITL
+    sitl = SITL()
+    sitl.download('copter', '3.3', verbose=True)
+    sitl_args = ['-I0', '--model', 'quad', '--home=-35.363261,149.165230,584,353']
+    sitl.launch(sitl_args, await_ready=True, restart=True)
+    connection_string='tcp:127.0.0.1:5760'
 
 
 # Connect to the Vehicle
-print 'Connecting to vehicle on: %s' % args.connect
-vehicle = connect(args.connect, wait_ready=True)
+print 'Connecting to vehicle on: %s' % connection_string
+vehicle = connect(connection_string, wait_ready=True)
 
 #global vehicle
-
-
-def scatterplot(x,y):
-    pyplot.plot(x,y,'b.')
-    pyplot.xlim(min(x)-1,max(x)+1)
-    pyplot.ylim(min(y)-1,max(y)+1)
-    pyplot.show()
 
 
 def cur_usec():
@@ -138,100 +58,61 @@ class MeasureTime(object):
     def reset(self):
         self.maxinterval = 0
         self.mininterval = 10000
+        
+    def log(self):
+        #print "Interval", self.previnterval
+        #print "MaxInterval", self.maxinterval
+        #print "MinInterval", self.mininterval
+        sys.stdout.write('MaxInterval: %s\tMinInterval: %s\tInterval: %s\r' % (self.maxinterval,self.mininterval, self.previnterval) )
+        sys.stdout.flush()
+
 
     def update(self):
         now = cur_usec()
         self.numcount = self.numcount + 1
         self.previnterval = now - self.prevtime
         self.prevtime = now
-        self.maxinterval = max(self.previnterval, self.maxinterval)
-        self.mininterval = min(self.mininterval, self.previnterval)
-
-        #print "Interval", self.previnterval
-        if (self.numcount % 100) == 0:
-            if self.numcount == 200:
-                # Ignore delays during startup
-                self.reset()
-            print "Interval", self.previnterval
-            print "MaxInterval", self.maxinterval
-            print "MinInterval", self.mininterval
+        if self.numcount>1: #ignore first value where self.prevtime not reliable.
+            self.maxinterval = max(self.previnterval, self.maxinterval)
+            self.mininterval = min(self.mininterval, self.previnterval)
+            self.log()
 
 
 acktime = MeasureTime()
 
-def mavrx_debug_handler(message):
-    """Measure heartbeat periodically"""
-    mtype = message.get_type()
-    global sendtime
 
-    #if mtype == 'HEARTBEAT':
-    if mtype == 'COMMAND_ACK':
-        #traceback.print_stack()
-        #print "GOT ACK", message
-        acktime.update()
-        send_testpackets()
+#Create COMMAND_ACK message listener.
+@vehicle.on_message('COMMAND_ACK')
+def listener(self, name, message):
+    acktime.update()
+    send_testpackets()
 
 
 def send_testpackets():
-        #print "send ROI cmds"
+    #Send message using `command_long_encode` (returns an ACK)
+    msg = vehicle.message_factory.command_long_encode(
+                                                    1, 1,    # target system, target component
+                                                    #mavutil.mavlink.MAV_CMD_DO_SET_RELAY, #command
+                                                    mavutil.mavlink.MAV_CMD_DO_SET_ROI, #command
+                                                    0, #confirmation
+                                                    0, 0, 0, 0, #params 1-4
+                                                    0,
+                                                    0,
+                                                    0
+                                                    )
 
-        # create the SET_POSITION_TARGET_GLOBAL_INT command
-        msg = vehicle.message_factory.set_position_target_global_int_encode(
-                                                                                 0,       # time_boot_ms (not used)
-                                                                                 1, 1,    # target system, target component
-                                                                                 mavutil.mavlink.MAV_FRAME_GLOBAL, # frame
-                                                                                 0b1000000011000111,       # type_mask - enable velocity only
-                                                                                 0, 0, 0, # x, y, z positions (not used)
-                                                                                 0, 0, 0.0, # x, y, z velocity in m/s
-                                                                                 0, 0, 0, # x, y, z acceleration (not used)
-                                                                                 0, 0)    # yaw, yaw_rate (not used)
+    vehicle.send_mavlink(msg)
 
-        # send command to vehicle
-        vehicle.send_mavlink(msg)
+#Start logging by sending a test packet
+send_testpackets()
 
-        # set ROI
-        msg = vehicle.message_factory.command_long_encode(
-                                                        1, 1,    # target system, target component
-                                                        #mavutil.mavlink.MAV_CMD_DO_SET_RELAY, #command
-                                                        mavutil.mavlink.MAV_CMD_DO_SET_ROI, #command
-                                                        0, #confirmation
-                                                        0, 0, 0, 0, #params 1-4
-                                                        0,
-                                                        0,
-                                                        0
-                                                        )
+print "Logging for 30 seconds"
+for x in range(1,30):
+    time.sleep(1)
 
-        vehicle.send_mavlink(msg)
+# Close vehicle object before exiting script
+vehicle.close()
 
-
-# Print out some interesting stats about the vehicle
-print "Mode: %s" % vehicle.mode
-print "Location: %s" % vehicle.location
-print "Attitude: %s" % vehicle.attitude
-print "Velocity: %s" % vehicle.velocity
-print "GPS: %s" % vehicle.gps_0
-print "Armed: %s" % vehicle.armed
-print "groundspeed: %s" % vehicle.groundspeed
-print "airspeed: %s" % vehicle.airspeed
-
-import time
-time.sleep(30)
-
-# Use of the following method is not recommended (it is better to add observer callbacks to attributes) but if you need it
-# it is available...
-vehicle.set_mavlink_callback(mavrx_debug_handler)
-
-# You can read and write parameters
-#print "Param: %s" % vehicle.parameters['THR_MAX']
-
-# Now download the vehicle waypoints
-cmds = vehicle.commands
-cmds.download()
-cmds.wait_ready()
-print "Home WP: %s" % cmds[0]
-print "Current dest: %s" % cmds.next
-
-
-print "Disarming..."
-vehicle.armed = False
-
+if not args.connect:
+    # Shut down simulator if it was started.
+    sitl.stop()
