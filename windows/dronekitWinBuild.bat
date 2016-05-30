a4f54b65a1e40c6cae47e72919a48f799cc9c827
Dr.-Ing. Amilcar Do Carmo Lucas <amilcar.lucas@iav.de>
2016-05-30 17:17:43 +0200

    Fix windows installer, it is called dronekit, not droneapi. Improve documentation a bit

--- windows/dronekitWinBuild.bat
+++ windows/dronekitWinBuild.bat
@@ -1,5 +1,6 @@
 rem build the standalone Dronekit.exe for Windows.
-rem This assumes Python is installed in C:\Python27
+rem This assumes Python2 is installed in C:\Python27
+rem This assumes Inno Setup 5 is installed in C:\Program Files (x86)\Inno Setup 5
 SETLOCAL enableextensions
 
 rem get the version
