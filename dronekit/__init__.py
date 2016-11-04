832b05aa59971404e4907ca58311d13bdddf2c87
Will Silva <will.silva@3drobotics.com>
2016-11-04 09:50:53 -0700

    Return 3D distance to home if `down` is known, otherwise return 2D distance
    * also, rename `distance` to `distance_home`

--- dronekit/__init__.py
+++ dronekit/__init__.py
@@ -184,12 +184,16 @@ class LocationLocal(object):
     def __str__(self):
         return "LocationLocal:north=%s,east=%s,down=%s" % (self.north, self.east, self.down)
 
-    def distance(self):
+    def distance_home(self):
         """
-        Distance away from home, in meters.
+        Distance away from home, in meters. Returns 3D distance if `down` is known, otherwise 2D distance.
         """
-        if self.north is not None and self.east is not None and self.down is not None:
-            return math.sqrt(self.north**2 + self.east**2 + self.down**2)
+
+        if self.north is not None and self.east is not None:
+            if self.down is not None:
+                return math.sqrt(self.north**2 + self.east**2 + self.down**2) 
+            else:
+                return math.sqrt(self.north**2 + self.east**2)
 
 
 class GPSInfo(object):
