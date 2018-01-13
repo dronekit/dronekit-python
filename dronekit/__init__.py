7c0ab3228d844479e9c84df41fd1961170a16803
pietrodn <powerpdn@gmail.com>
2018-01-13 19:30:04 +0100

    Fix bug causing intermittent timeouts on Python 3

--- dronekit/__init__.py
+++ dronekit/__init__.py
@@ -1290,7 +1290,7 @@ class Vehicle(HasObservers):
                 c = 0
                 for i, v in enumerate(self._params_set):
                     if v == None:
-                        self._master.mav.param_request_read_send(0, 0, '', i)
+                        self._master.mav.param_request_read_send(0, 0, b'', i)
                         c += 1
                         if c > 50:
                             break
