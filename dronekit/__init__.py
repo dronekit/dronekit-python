32b6ba10713b35f8cd61492bab5e5235c5e149cc
michael-hart <michael-hart@users.noreply.github.com>
2018-05-15 15:27:52 +0100

    Home location set command corrected
    param1 should have value 0 rather than 2

--- dronekit/__init__.py
+++ dronekit/__init__.py
@@ -1969,7 +1969,7 @@ class Vehicle(HasObservers):
             0, 0,  # target system, target component
             mavutil.mavlink.MAV_CMD_DO_SET_HOME,  # command
             0,  # confirmation
-            2,  # param 1: 1 to use current position, 2 to use the entered values.
+            0,  # param 1: 1 to use current position, 0 to use the entered values.
             0, 0, 0,  # params 2-4
             pos.lat, pos.lon, pos.alt))
 
