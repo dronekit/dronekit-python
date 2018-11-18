b1b91841683c0360f2463625750b1ccfd54c11e0
pietrodn <powerpdn@gmail.com>
2018-11-18 16:19:16 +0100

    Add commands and tests for sensor calibration

---
+++ dronekit/test/sitl/test_sensor_calibration.py
@@ -0,0 +1,95 @@
+from pymavlink import mavutil
+
+from dronekit import connect
+from dronekit.test import with_sitl
+
+from dronekit.test.sitl import assert_command_ack
+
+
+@with_sitl
+def test_gyro_calibration(connpath):
+    """Request gyroscope calibration, and check for the COMMAND_ACK."""
+
+    vehicle = connect(connpath, wait_ready=True)
+
+    with assert_command_ack(vehicle, mavutil.mavlink.MAV_CMD_PREFLIGHT_CALIBRATION, timeout=30):
+        vehicle.calibrate_gyro()
+
+    vehicle.close()
+
+
+@with_sitl
+def test_magnetometer_calibration(connpath):
+    """Request magnetometer calibration, and check for the COMMAND_ACK."""
+
+    vehicle = connect(connpath, wait_ready=True)
+
+    with assert_command_ack(
+        vehicle,
+        mavutil.mavlink.MAV_CMD_DO_START_MAG_CAL,
+        timeout=30,
+        ack_result=mavutil.mavlink.MAV_RESULT_UNSUPPORTED,  # TODO: change when APM is upgraded
+    ):
+        vehicle.calibrate_magnetometer()
+
+    vehicle.close()
+
+
+@with_sitl
+def test_simple_accelerometer_calibration(connpath):
+    """Request simple accelerometer calibration, and check for the COMMAND_ACK."""
+
+    vehicle = connect(connpath, wait_ready=True)
+
+    with assert_command_ack(
+        vehicle,
+        mavutil.mavlink.MAV_CMD_PREFLIGHT_CALIBRATION,
+        timeout=30,
+        ack_result=mavutil.mavlink.MAV_RESULT_FAILED,  # TODO: change when APM is upgraded
+    ):
+        vehicle.calibrate_accelerometer_simple()
+
+    vehicle.close()
+
+
+@with_sitl
+def test_accelerometer_calibration(connpath):
+    """Request accelerometer calibration, and check for the COMMAND_ACK."""
+
+    vehicle = connect(connpath, wait_ready=True)
+
+    # The calibration is expected to fail because in the SITL we don't tilt the Vehicle.
+    # We just check that the command isn't denied or unsupported.
+    with assert_command_ack(
+        vehicle,
+        mavutil.mavlink.MAV_CMD_PREFLIGHT_CALIBRATION,
+        timeout=30,
+        ack_result=mavutil.mavlink.MAV_RESULT_FAILED,
+    ):
+        vehicle.calibrate_accelerometer()
+
+    vehicle.close()
+
+
+@with_sitl
+def test_board_level_calibration(connpath):
+    """Request board level calibration, and check for the COMMAND_ACK."""
+
+    vehicle = connect(connpath, wait_ready=True)
+
+    with assert_command_ack(vehicle, mavutil.mavlink.MAV_CMD_PREFLIGHT_CALIBRATION, timeout=30):
+        vehicle.calibrate_board_level()
+
+    vehicle.close()
+
+
+@with_sitl
+def test_barometer_calibration(connpath):
+    """Request barometer calibration, and check for the COMMAND_ACK."""
+
+    vehicle = connect(connpath, wait_ready=True)
+
+    with assert_command_ack(vehicle, mavutil.mavlink.MAV_CMD_PREFLIGHT_CALIBRATION, timeout=30):
+        vehicle.calibrate_barometer()
+
+    vehicle.close()
