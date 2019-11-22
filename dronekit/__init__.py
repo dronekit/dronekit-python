913a8a03d61ff497d6978fc8b9647f45fa490023
Kiran Shila <kiranshila@Kirans-MacBook-Pro.local>
2019-11-22 11:20:06 -0500

    Updated heartbeat on_message to ignore gimbal heartbeats

--- dronekit/__init__.py
+++ dronekit/__init__.py
@@ -1209,7 +1209,7 @@ class Vehicle(HasObservers):
         @self.on_message('HEARTBEAT')
         def listener(self, name, m):
             # ignore groundstations
-            if m.type == mavutil.mavlink.MAV_TYPE_GCS:
+            if m.type == mavutil.mavlink.MAV_TYPE_GCS or m.type == mavutil.mavlink.MAV_TYPE_GIMBAL:
                 return
             self._armed = (m.base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED) != 0
             self.notify_attribute_listeners('armed', self.armed, cache=True)
